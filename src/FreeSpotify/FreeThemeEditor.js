937be420a747fb744a5ce06dfda3880cb4b5e81e
watermelonPop <SK21007@Tamu.edu>
2025-07-16 17:55:58 -0500

    more theme creator edits

--- src/FreeSpotify/FreeThemeEditor.js
+++ src/FreeSpotify/FreeThemeEditor.js
@@ -128,41 +128,58 @@ function FreeThemeEditor({selectedEditTheme, themes, setThemes, currentTheme, se
                         }}><FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon></button>
                         <p className="title">Edit Theme</p>
                 </div>
-                <div className='themeColorInputDiv'>
+                <div className='themeCreatorForm'>
+                <div className='themeNameInputDiv'>
                         <label htmlFor="name">Name: </label>
                         <input id="name" type="text" value={buildTheme.name} onChange={(e) => handleThemeChange("name", e.target.value)}></input>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="backgroundColor1">Background Color 1: </label>
-                        <input id="backgroundColor1" type="color" value={buildTheme.backgroundColor1} onChange={(e) => handleThemeChange("backgroundColor1", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="backgroundColor1" type="color" value={buildTheme.backgroundColor1} onChange={(e) => handleThemeChange("backgroundColor1", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="txtColor1">Text Color 1: </label>
-                        <input id="txtColor1" type="color" value={buildTheme.txtColor1} onChange={(e) => handleThemeChange("txtColor1", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="txtColor1" type="color" value={buildTheme.txtColor1} onChange={(e) => handleThemeChange("txtColor1", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="backgroundColor2">Background Color 2: </label>
-                        <input id="backgroundColor2" type="color" value={buildTheme.backgroundColor2} onChange={(e) => handleThemeChange("backgroundColor2", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="backgroundColor2" type="color" value={buildTheme.backgroundColor2} onChange={(e) => handleThemeChange("backgroundColor2", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="txtColor2">Text Color 2: </label>
-                        <input id="txtColor2" type="color" value={buildTheme.txtColor2} onChange={(e) => handleThemeChange("txtColor2", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="txtColor2" type="color" value={buildTheme.txtColor2} onChange={(e) => handleThemeChange("txtColor2", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="backgroundColor3">Background Color 3: </label>
-                        <input id="backgroundColor3" type="color" value={buildTheme.backgroundColor3} onChange={(e) => handleThemeChange("backgroundColor3", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="backgroundColor3" type="color" value={buildTheme.backgroundColor3} onChange={(e) => handleThemeChange("backgroundColor3", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="txtColor3">Text Color 3: </label>
-                        <input id="txtColor3" type="color" value={buildTheme.txtColor3} onChange={(e) => handleThemeChange("txtColor3", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="txtColor3" type="color" value={buildTheme.txtColor3} onChange={(e) => handleThemeChange("txtColor3", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="accentColor">Accent Color: </label>
-                        <input id="accentColor" type="color" value={buildTheme.accentColor} onChange={(e) => handleThemeChange("accentColor", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="accentColor" type="color" value={buildTheme.accentColor} onChange={(e) => handleThemeChange("accentColor", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeColorInputDiv'>
                         <label htmlFor="accentTxtColor">Accent Text Color: </label>
-                        <input id="accentTxtColor" type="color" value={buildTheme.accentTxtColor} onChange={(e) => handleThemeChange("accentTxtColor", e.target.value)}></input>
+                        <div className="colorInputWrapper">
+                                <input id="accentTxtColor" type="color" value={buildTheme.accentTxtColor} onChange={(e) => handleThemeChange("accentTxtColor", e.target.value)}></input>
+                        </div>
                 </div>
                 <div className='themeSelectInputDiv'>
                         <label htmlFor="font">Font: </label>
@@ -178,7 +195,8 @@ function FreeThemeEditor({selectedEditTheme, themes, setThemes, currentTheme, se
                         <p id="fontWeight">{buildTheme.fontWeight}</p>
                         <button onClick={() => adjustFontWeight(+100)}>+</button>
                 </div>
-                <button className='createThemeBtn' onClick={() => {
+                </div>
+                <button className='settingsBtns' onClick={() => {
                 handleEditTheme(buildTheme);
 
                 // ✅ If the edited theme was the current theme, reapply it
@@ -193,7 +211,7 @@ function FreeThemeEditor({selectedEditTheme, themes, setThemes, currentTheme, se
 
                 setSelectedEditTheme(null);
                 }}>Save</button>
-                <button className='createThemeBtn' onClick={() => {
+                <button className='settingsBtns themeCreatorBtn' onClick={() => {
                         handleDeleteTheme(selectedEditTheme); // handles fallback logic
                         setSelectedEditTheme(null); // close editor
                         }}>DELETE</button>
